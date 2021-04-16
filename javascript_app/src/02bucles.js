4fdfa3c461562d09e117e7db5fcedd6c12ac31f2
josegandias <gandiaspascual@gmail.com>
2021-04-16 10:48:53 -0500

    javascrip

---
+++ javascript_app/src/02bucles.js
@@ -0,0 +1,31 @@
+let limiteBucle = 10;
+let contador = 0;
+
+console.log('+ Bucle: Do While');
+do {
+    contador ++;
+    console.log('Contador: '+contador);
+} while (contador < limiteBucle);
+
+console.log('+ Bucle: Do While');
+let letCondicion = true;
+
+while (letCondicion) {
+    let letRandom = Math.random() * 10;
+    console.log('Numero Randomico: '+letRandom);
+    if (letRandom > 5) {
+        letCondicion = false;
+        console.log('Salida del Bucle: '+letCondicion);
+    }
+}
+
+console.log('+ Bucle: For');
+
+let limiteFor = 20;
+
+console.log('Limite = '+limiteFor);
+for (let index = 0; index < limiteFor; index++) {
+    let randomFor =  Number.parseInt((Math.random() * 10),10);
+    console.log('Indice for: '+index);
+    console.log('Random    : '+randomFor);    
+}
